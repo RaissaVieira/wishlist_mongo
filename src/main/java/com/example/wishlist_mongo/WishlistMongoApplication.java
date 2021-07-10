17f6c9fe59f5996db2298702968943b9cb74c5b1
Raissa Vieira <raissa.vieira@lavid.ufpb.br>
2021-07-10 10:11:26 -0300

    iniciando projeto spring

---
+++ src/main/java/com/example/wishlist_mongo/WishlistMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlist_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistMongoApplication.class, args);
+    }
+
+}
