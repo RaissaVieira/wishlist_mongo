fa81644d709dd6185e41a9d321cf59edd33ec4b8
Raissa Vieira <raissa.vieira@lavid.ufpb.br>
2021-07-10 17:24:10 -0300

    configurando swagger

--- src/main/java/com/example/wishlist_mongo/config/SwaggerConfig.java
+++ src/main/java/com/example/wishlist_mongo/config/SwaggerConfig.java
@@ -1,2 +1,44 @@
-package com.example.wishlist_mongo.config;public class SwaggerConfig {
+package com.example.wishlist_mongo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        ApiInfo apiInfo =  new ApiInfo(
+                "Wishlist Magalu",
+                "REST API designed to resolve client wishlist functionality.",
+                "1.0",
+                "Terms Of Service",
+                new Contact("Raissa da Silva Vieira", "", "raissavieira125@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/license.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+
 }
